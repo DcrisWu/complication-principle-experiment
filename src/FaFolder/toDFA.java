f7fcfb4cd85745ba2d47b173577c439a9fb5b0fc
Dcris <18218552814@163.com>
2022-11-06 14:33:12 +0800

    deleted:    .idea/vcs.xml 	modified:   src/FaFolder/toDFA.java

--- src/FaFolder/toDFA.java
+++ src/FaFolder/toDFA.java
@@ -183,18 +183,3 @@ public class toDFA {
     }
 }
 
-/*
-class MyHashSet<E> extends HashSet<E> {
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Set<?>)) return false;
-        Set<?> o1 = (Set<?>) o;
-        if (this.size() != o1.size()) return false;
-        boolean judge = true;
-        for (Object o2 : o1) {
-            if (!this.contains(02)) return false;
-        }
-        return true;
-    }
-}*/
